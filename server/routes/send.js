33633ae217ecb0c88d158bd2d8bec5594d87d3fb
Xen1k <xen1kdevelop@gmail.com>
2022-06-30 18:27:07 +0300

    Changed Port

--- server/routes/send.js
+++ server/routes/send.js
@@ -4,15 +4,17 @@ const nodemailer = require("nodemailer");
 
 router.post('/send', async (req, res) => {
 
+
     let transporter = nodemailer.createTransport({
         service: req.body.senderService,
-        port: 587,
-        secure: false, 
+        port: 584,
+        // secure: false, 
         auth: {
             user: req.body.senderEmail,
             pass: req.body.senderPassword
         },
     });
+    console.log(req.body.senderPassword)
 
     let info = await transporter.sendMail({
         from: req.body.senderEmail, 
